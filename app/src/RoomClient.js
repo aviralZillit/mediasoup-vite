5c27569e26d2a8843e016519e2d452675fa4964a
AviralZillit <147372963+aviralZillit@users.noreply.github.com>
2025-04-17 15:17:03 +0530

    removed room client changes

--- app/src/RoomClient.js
+++ app/src/RoomClient.js
@@ -11,7 +11,6 @@ const VIDEO_CONSTRAINS = {
 	qvga : { width: { ideal: 320 }, height: { ideal: 240 } },
 	vga  : { width: { ideal: 640 }, height: { ideal: 480 } },
 	hd   : { width: { ideal: 1280 }, height: { ideal: 720 } }
-	
 };
 
 const PC_PROPRIETARY_CONSTRAINTS = {
