cb82d64333b69b2a0fe5fcdf5bf2e1fdd623580a
Aviral Singh Pal <147372963+aviralZillit@users.noreply.github.com>
2025-12-15 19:32:57 +0530

    Refactor URL factory: improve code readability by adjusting formatting and removing commented-out code

--- app/src/urlFactory.js
+++ app/src/urlFactory.js
@@ -2,7 +2,8 @@ import qs from 'qs';
 
 let protooPort = 4443;
 
-if (window.location.hostname === 'test.mediasoup.org') {
+if (window.location.hostname === 'test.mediasoup.org') 
+{
 	protooPort = 4444;
 }
 
@@ -10,10 +11,33 @@ const hostname = window.location.hostname;
 const protocol = 'wss';
 
 // const hostname = 'v3demo.mediasoup.org'
+// const hostname = 'calling-sfu-qa.zillit.com'
 // const protocol = 'ws'
 
-export function getProtooUrl(params) {
+export function getProtooUrl(params) 
+{
 	const query = qs.stringify(params);
 
 	return `${protocol}://${hostname}:${protooPort}/?${query}`;
 }
+
+// for testing QA purpose only
+
+// if (window.location.hostname === 'test.mediasoup.org') 
+// {
+// 	protooPort = 4444;
+// }
+
+// // const hostname = window.location.hostname;
+// const protocol = 'wss';
+
+// // const hostname = 'v3demo.mediasoup.org'
+// const hostname = 'calling-sfu-qa.zillit.com';
+// // const protocol = 'ws'
+
+// export function getProtooUrl(params) 
+// {
+// 	const query = qs.stringify(params);
+
+// 	return `${protocol}://${hostname}/?${query}`;
+// }
